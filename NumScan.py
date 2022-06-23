1db6bd66ca16fa7adda48aade4e219658a2218b8
6virus <104843039+0xffvirus@users.noreply.github.com>
2022-06-24 00:27:29 +0300

    Update NumScan.py

--- NumScan.py
+++ NumScan.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import requests, os, sys
 from termcolor import colored
 os.system('color')
@@ -18,12 +18,12 @@ def start(usr):
         url = "http://146.148.112.105/caller/index.php/UserManagement/search_number?number="+usr+"&country_code=SA"
         r = requests.get(url)
         num = str(r.json()['result'][0]['number'])
-        name = str(r.json()['result'][0]['name']).encode("utf-8")
+        name = str(r.json()['result'][0]['name'])
         country = str(r.json()['result'][0]['country_code'])
         id = str(r.json()['result'][0]['id'])
         print(colored("---- Coded By 6Virus ----", 'red'))
         print(colored("[!]", 'green'),colored("number :", 'cyan'),num)
-        print(colored("[!]", 'green'),colored("name :",'cyan'),sys.stdout.buffer.write(name))
+        print(colored("[!]", 'green'),colored("name :",'cyan'),name)
         print(colored("[!]", 'green'),colored("Country :", 'cyan'),country)
         print(colored("[!]", 'green'),colored("id :", 'cyan'),id)
         input("Press Any Key to Close ...")
